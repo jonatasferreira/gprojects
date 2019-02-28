a76afc049a1ffbf928dbe72aa78926b31f93e8ca
Jonatas Ferreira <jonatas402@gmail.com>
2019-02-27 22:29:22 -0300

    Adicionado aviso de nenhum cadastrado.

--- application/views/client/index.php
+++ application/views/client/index.php
@@ -4,20 +4,26 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <div class="container">
 	<h1>Gereciador de Projetos</h1>
 	<h3>Cliente</h3>
-	<table class="table">
-		<tr>
-			<th>CPF</th>
-			<th>Nome</th>
-			<th>Editar</th>
-			<th>Deletar</th>
-		</tr>
-		<?php foreach ($clientList as $client) : ?>
-		<tr>
-			<td><?php echo $client->getCpf();?></td>
-			<td><?php echo $client->getName();?></td>
-			<td><a href="cliente/atualizar/<?php echo $client->getCpf();?>">editar</a></td>
-			<td><a href="cliente/deletar/<?php echo $client->getCpf();?>">deletar</a></td>
-		</tr>
-		<?php endforeach; ?>
-	</table>
+	<?php if (isset($clientList) && count($clientList)) : ?>
+		<table class="table">
+			<tr>
+				<th>CPF</th>
+				<th>Nome</th>
+				<th>Editar</th>
+				<th>Deletar</th>
+			</tr>
+			<?php foreach ($clientList as $client) : ?>
+			<tr>
+				<td><?php echo $client->getCpf();?></td>
+				<td><?php echo $client->getName();?></td>
+				<td><a href="cliente/atualizar/<?php echo $client->getCpf();?>">editar</a></td>
+				<td><a href="cliente/deletar/<?php echo $client->getCpf();?>">deletar</a></td>
+			</tr>
+			<?php endforeach; ?>
+		</table>
+	<?php else : ?>
+		<div class="alert alert-warning" role="alert">
+		Nenhum cliente encontrado.
+		</div>
+	<?php endif; ?>
 </div>
--- application/views/employee/index.php
+++ application/views/employee/index.php
@@ -4,20 +4,26 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <div class="container">
 	<h1>Gereciador de Projetos</h1>
 	<h3>Funcionários</h3>
-	<table class="table">
-		<tr>
-			<th>CPF</th>
-			<th>Nome</th>
-			<th>Editar</th>
-			<th>Deletar</th>
-		</tr>
-		<?php foreach ($employeeList as $employee) : ?>
-		<tr>
-			<td><?php echo $employee->getCpf();?></td>
-			<td><?php echo $employee->getName();?></td>
-			<td><a href="funcionario/atualizar/<?php echo $employee->getCpf();?>">editar</a></td>
-			<td><a href="funcionario/deletar/<?php echo $employee->getCpf();?>">deletar</a></td>
-		</tr>
-		<?php endforeach; ?>
-	</table>
+	<?php if (isset($employeeList) && count($employeeList)) : ?>
+		<table class="table">
+			<tr>
+				<th>CPF</th>
+				<th>Nome</th>
+				<th>Editar</th>
+				<th>Deletar</th>
+			</tr>
+			<?php foreach ($employeeList as $employee) : ?>
+			<tr>
+				<td><?php echo $employee->getCpf();?></td>
+				<td><?php echo $employee->getName();?></td>
+				<td><a href="funcionario/atualizar/<?php echo $employee->getCpf();?>">editar</a></td>
+				<td><a href="funcionario/deletar/<?php echo $employee->getCpf();?>">deletar</a></td>
+			</tr>
+			<?php endforeach; ?>
+		</table>
+	<?php else : ?>
+		<div class="alert alert-warning" role="alert">
+		Nenhum funcionário encontrado.
+		</div>
+	<?php endif; ?>
 </div>
